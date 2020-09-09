94fca12903b4c84851f6e45e8d2e53de500fc64c
Adanetx <Adane1301992@gmail.com>
2020-09-09 16:03:47 -0500

    categorry updated

--- src/components/routes/Categories.js
+++ src/components/routes/Categories.js
@@ -27,7 +27,6 @@ class Categories extends Component {
         'Authorization': `Token token=${this.props.user.token}`
       }
     })
-
       .then(res => this.setState({ categories: res.data.categories }))
     // .then(res => console.log(res))
       .catch(res => this.props.msgAlert({
